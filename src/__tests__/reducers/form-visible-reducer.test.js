cb470ac45f9060c779a80277a96b163077b62c01
unknown <macjay314@gmail.com>
2020-07-10 09:09:58 -0700

    Add second test to toggle form.

--- src/__tests__/reducers/form-visible-reducer.test.js
+++ src/__tests__/reducers/form-visible-reducer.test.js
@@ -5,4 +5,8 @@ describe("formVisibleReducer", () => {
   test('Should return to default state if action type is not one of the predefined actions', () => {
     expect(formVisibleReducer(false, { type: null })).toEqual(false);
   });
+
+  test('Should successfully toggle form state from false to true', () => {
+    expect(formVisibleReducer(false, { type : 'TOGGLE_FORM'})).toEqual(true);
+  });
 })
