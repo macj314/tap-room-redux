99819c029895ceb275d5fc5451487daacc24c1c4
unknown <macjay314@gmail.com>
2020-07-10 10:08:24 -0700

    Create and test deleteBag and addBag.

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -1,4 +1,4 @@
-import * as actions from './../../actions';
+import * as actions from '../../actions';
 
 describe('form actions', () => {
   test('toggle form should create TOGGLE_FORM action', () => {
@@ -6,4 +6,28 @@ describe('form actions', () => {
       type: 'TOGGLE_FORM'
     });
   });
+
+  test('deleteBag should call DELETE_BAG action', () => {
+    expect(actions.deleteBag(1)).toEqual({
+      type: 'DELETE_BAG',
+      id: 1
+    });
+  });
+
+  test('addBag should call ADD_BAG action', () => {
+    expect(actions.addBag({
+      name: 'Jays Mocha',
+      roast: 'light',
+      flavor: 'Chocolate',
+      price: 12.00,
+      id: 1
+    })).toEqual({
+      type: 'ADD_BAG',
+      name: 'Jays Mocha',
+      roast: 'light',
+      flavor: 'Chocolate',
+      price: 12.00,
+      id: 1
+      });
+  })
 })
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,20 @@
 export const toggleForm = () => ({
   type: 'TOGGLE_FORM'
 });
+
+export const deleteBag = id => ({
+  type: 'DELETE_BAG',
+  id
+});
+
+export const addBag = (bag) => {
+  const {name, roast, flavor, price, id} = bag
+  return {
+    type: 'ADD_BAG',
+    name: name,
+    roast: roast,
+    flavor: flavor,
+    price: price,
+    id: id
+  }
+}
